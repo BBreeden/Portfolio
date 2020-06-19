5a83c278b13e39abf9ca96dfcac41c711b91abe9
Brian Breeden <breedenb@vt.edu>
2020-06-19 17:21:22 -0400

    finished first iteration of modal design.

--- static/js/modals.js
+++ static/js/modals.js
@@ -6,6 +6,7 @@ var modal_header = document.getElementById('modal_header');
 var modal_body = document.getElementById('modal_body');
 
 var com_rep_button = document.getElementById('com_reports');
+var login_tmps_button = document.getElementById('login_tmps');
 
 close_span.onclick = function (){
     modal.style.display = 'none';
@@ -18,7 +19,19 @@ window.onclick = function(event){
 }
 
 com_rep_button.onclick = function() {
-    modal.style.display = 'block';
-    modal_header.innerHTML = 'This is a test modal header!';
-    modal_body.innerHTML = 'This is a test modal body!';
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+    modal.style.alignItems = 'center';
+    modal.style.justifyContent = 'center';
+    modal_header.innerHTML = 'Commercial Reports';
+    modal_body.innerHTML = 'Commercial reporting allows our users to gain data driven insight to their business. As of this writing, there are eight different report types that fall under this category. As the lead Quality Engineer on this project, I tested the report service distribution behaviors, the user interface, and the integrity of the data that made up each respective report. <br /><br />This feature is currently live to our commercial customers. View a sample report.';
+}
+
+login_tmps_button.onclick = function() {
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+    modal.style.alignItems = 'center';
+    modal.style.justifyContent = 'center';
+    modal_header.innerHTML = 'Login Templates';
+    modal_body.innerHTML = 'For each security system, there are logins that a user can configure for other individuals to log into their system. This is a key part of the commercial domain due to the need to allow employees and other support staff access to the security system. This project allows the users to create templates for their logins. This enables our users to same time and maintain consistency across different login types. <br /><br />This feature is currently live to our commercial customers.';
 }
