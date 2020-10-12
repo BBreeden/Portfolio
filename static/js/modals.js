1449febfd4d52c7cf3ed8373c70a17a5b316fceb
Brian Breeden <breedenb@vt.edu>
2020-10-12 10:21:54 -0400

    review of work project descriptions.

--- static/js/modals.js
+++ static/js/modals.js
@@ -32,7 +32,7 @@ com_rep_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Commercial Reports';
-    modal_body.innerHTML = 'Commercial reporting allows our users to gain data driven insight to their business. As of this writing, there are eight different report types that fall under this category. As the lead Quality Engineer on this project, I tested the report service distribution behaviors, the user interface, and the integrity of the data that made up each respective report.';
+    modal_body.innerHTML = 'Commercial reporting allows our users to gain data-driven insight to their business. As of this writing, there are eight different report types that fall under this category. As the lead Quality Engineer on this project, I tested the report service distribution behaviors. In addition, I ensured the user interface and data integrity met all expectations.';
 }
 
 login_tmps_button.onclick = function() {
@@ -41,7 +41,7 @@ login_tmps_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Login Templates';
-    modal_body.innerHTML = 'For each security system, there are logins that a user can configure for other individuals to log into their system. This is a key part of the commercial domain due to the need to allow employees and other support staff access to the security system. This project allows the users to create templates for their logins. This enables our users to save time and maintain consistency across different login types. <br /><br />As the lead QE on this project I leveraged a complex text plan to test various login template combinations, verified user perissions across different system levels, and monitored performance on stressed systems.';
+    modal_body.innerHTML = 'For each security system, there are logins associated with that system. The user can configure these logins for a variety of different uses. The Login Templates feature allows the user to configure a single template and assign the template to multiple logins. This enables our users to save time and maintain consistency across different logins. <br /><br />As the lead QE on this project, I leveraged a complex text plan to test various login template combinations. I verified user permissions across different system levels and monitored performance on stressed systems.';
 }
 
 nuke_button.onclick = function() {
@@ -50,7 +50,7 @@ nuke_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Point Hub';
-    modal_body.innerHTML = 'As the lead integration QE for this project, I collaborated with the Security Panels team to develop a new generation of security hub for our PointCentral partner. The Point Hub is a security hub that takes advantage of a dedicated cell connection, allowing the device to operate even if there is a loss of power and/or broadband signals. <br /><br /> <a href="https://www.pointcentral.com/hardware-partners/" target="_blank">Learn more about the Point Hub.</a>';
+    modal_body.innerHTML = 'Security hubs are the center of any security system. They allow the security devices to communicate with each other. This includes locks, thermostats, contact sensors, and video cameras. <br /><br /> As the lead Commercial QE for this project, I collaborated with the Security Panels team to develop a new generation of security hub for our PointCentral partner. The Point Hub is a security hub that takes advantage of a dedicated cell connection, allowing the device to operate even if there is a loss of power and/or broadband signals. The primary use case for this device is short term and long term rental properties. <br /><br /> <a href="https://www.pointcentral.com/hardware-partners/" target="_blank">Learn more about the Point Hub.</a>';
 }
 
 sbtm_button.onclick = function(){
@@ -59,7 +59,7 @@ sbtm_button.onclick = function(){
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Smarter Business Temperature Monitoring';
-    modal_body.innerHTML = 'Our commercial customers work in a variety of industries, including industries that leverage walk-in freezers, refrigeration units, and climate controlled areas. To assist them in gaining insight about these temperature sensitive areas, we developed the Smarter Business Temperature Monitoring system.<br /><br /> As the lead QE on this project, I extensively tested temperature sensor integration, temperature reporting, sensor dashboards, and analyzed overall feature performance.';
+    modal_body.innerHTML = 'Our commercial customers work in a variety of industries, including industries that leverage walk-in freezers, refrigeration units, and climate controlled areas. To our customers with gaining insight about these temperature sensitive areas, we developed the Smarter Business Temperature Monitoring system. <br /><br /> As the lead QE on this project, I tested temperature sensor integration, temperature reporting, sensor dashboards, and analyzed overall feature performance.';
 }
 
 vhr_button.onclick = function() {
@@ -68,7 +68,7 @@ vhr_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Video Health Report'
-    modal_body.innerHTML = 'One of the biggest concerns for our customers is security. The foundation of every good security system is the video camera. Out team developed a report that gives our customers insight on the status of their video cameras. This includes performance metrics, camera status, camera snapshots, and recording hardware updates (known as a <a href="https://www.alarmgrid.com/faq/how-does-an-alarm-dot-com-svr-work" rel="noreferrer" target="_blank">Stream Video Recorder</a>). <br /><br />As the lead on this quality project, I developed a test plan to verify each, individual performance metric. I also tested the report distribution systems and monitored performance with stressed video accounts.'
+    modal_body.innerHTML = 'One of the biggest concerns for our customers is security. The foundation of every good security system is the video camera. Our team developed a report that gives our customers insight on the status of their video cameras. This includes performance metrics, camera status, camera snapshots, and recording hardware updates (known as a <a href="https://www.alarmgrid.com/faq/how-does-an-alarm-dot-com-svr-work" rel="noreferrer" target="_blank">Stream Video Recorder</a>). <br /><br />As the quality lead on this project, I developed a test plan to verify each, individual performance metric. I also tested the report distribution systems and monitored performance with stressed video accounts.'
 }
 
 yale500_button.onclick = function() {
@@ -104,7 +104,7 @@ water_valve_meter_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Water Valve and Meter'
-    modal_body.innerHTML = 'Water damage costs $9.1 billion dollars every year. To help alleviate this problem, we released a Smart Water Valve & Meter. This allows our users to create automation rules to automatically turn off their water if a leak is detected. As the lead Quality Engineer on this project, I designed and executed an integration test plan. Working alongside our Building36 team, I tested all of the features of this device to ensure the integration with our <a href="https://www.pointcentral.com/" rel="noreferrer" target="_blank">PointCentral</a> partner functions as intended. <br /><br /> <a href="https://www.alarm.com/about/press/PressGeneric.aspx?cmid=670" rel="noreferrer" target="_blank">Learn more about the product here.</a> '
+    modal_body.innerHTML = 'Water damage costs $9.1 billion dollars every year. To help alleviate this problem, we released a Smart Water Valve & Meter. This allows our users to create automation rules to automatically turn off their water if a leak is detected. As the lead Quality Engineer on this project, I designed and executed an integration test plan. Working alongside our Building36 team, I tested all of the features of this device to ensure it integrated with our <a href="https://www.pointcentral.com/" rel="noreferrer" target="_blank">PointCentral</a> partner. <br /><br /> <a href="https://www.alarm.com/about/press/PressGeneric.aspx?cmid=670" rel="noreferrer" target="_blank">Learn more about the product here.</a> '
 }
 
 jasco_contact_button.onclick = function() {
@@ -113,5 +113,5 @@ jasco_contact_button.onclick = function() {
     modal.style.alignItems = 'center';
     modal.style.justifyContent = 'center';
     modal_header.innerHTML = 'Jasco Contact Sensor'
-    modal_body.innerHTML = 'The contact sensor is a staple in a security system. Alerting the user if an entry point is open or closed forms the centerpiece of our security and automation suite. This allows our users to configure their devices to put their system into an alarm if their business or property is accessed when no one is present. Additionally, users can configure automation rules to execute system actions if an entry point is accessed. As the lead Quality Engineer on this project, I composed and executed a full regression test suite to ensure this new generation of contact sensor meets our expectations for our commercial and property rental partners.'
+    modal_body.innerHTML = 'The contact sensor is a staple in a security system. The sensor alerts the user if an entry point is open or closed. This allows our users to configure their devices to put their system into an alarm if their business or property is accessed when no one is present. Additionally, users can configure a set of automation rules to execute system actions if the sensor detects activity. As the lead Quality Engineer on this project, I composed and executed a full regression test suite to ensure this new generation of contact sensor meets our expectations.'
 }
